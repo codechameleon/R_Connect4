94dfd2d963aab03b3db2672660a6bd995c9f5311
Michael Torres <torresmi@mail.gvsu.edu>
2013-04-14 16:15:37 -0400

    model and driver done

---
+++ c4_driver.rb
@@ -0,0 +1,71 @@
+require 'c4_model'
+
+
+class InvalidInput < ArgumentError
+end
+
+class C4Driver
+
+  # Defaults and optional command line inputs
+  row, column =  8, 8
+  length_to_win = 4
+  if ARGV.length > 0 and ARGV.length < 3
+    row,column = ARGV[0].to_i
+    length_to_win = ARGV[1].to_i
+
+  else if ARGV.length >= 3
+         row = ARGV[0].to_i
+         column = ARGV[1].to_i
+         length_to_win = ARGV[2].to_i
+       end
+  end
+
+  # Set up game
+  game = C4Model.new(row,column,length_to_win)
+  game.print_board
+  player = 0
+
+  catch(:quit) do
+  # play game
+    winner = nil
+    until winner do
+          turn = nil
+
+      # Take turn
+      until turn do
+        begin
+         print "Player ", player, " enter column."
+         picked = nil
+
+         # Error checking for input
+         until picked do
+            begin
+              input = STDIN.gets.chomp
+
+              throw :quit if input.eql?"quit"
+
+              picked = Integer(input)
+
+            rescue ArgumentError
+              print "Not an integer! Try again:"
+            end
+         end
+         # place token
+         if game.place_token(picked).nil?
+           raise InvalidInput
+         end
+         game.print_board
+         winner = game.who_won
+         turn = 1
+         player = (1 + player) % 2
+        rescue InvalidInput
+         puts "Invalid input. Try again"
+
+        end
+      end
+    end
+  print "Player ", player, " won!"
+  puts ""
+  end
+  puts "Thanks for playing"
+end
---
+++ c4_model.rb
@@ -0,0 +1,132 @@
+class C4Model
+
+
+  # Sets up the board
+  def initialize(row, column, win)
+    @board = Array.new(row) {Array.new(column)}
+    @current_player = 0
+    @win_length = win
+    @last_col = 0
+    @last_row = 0
+
+  end
+
+  # Places player token
+  def place_token(column)
+
+   unless column > @board[0].length-1
+      @board.each_index do |i|
+         if @board.reverse[i][column].nil?
+           @board.reverse[i][column] = @current_player
+           @current_player = (@current_player + 1) % 2
+           @last_col = column
+           @last_row = @board.size-1 - i
+           who_won
+           return true
+         end
+      end
+     nil
+    end
+  end
+
+  def who_won
+
+    catch (:match) do
+
+      # Checks the row of the last placed token for a win
+      x_in_a_row(@board[@last_row])
+
+      # Checks the column of the last inserted token for a win
+      array = []
+      @board.each_index do |i|
+        array.push(@board[i][@last_col])
+      end
+      x_in_a_row(array)
+
+      # Checks the forward diagonal of the last move  (\)
+      array.clear
+      start = @last_col - @last_row
+      # If we need to check the top half or middle of the board
+      if start >= 0
+        (start .. @board.length-1).each_with_index do |i, index|
+          array.push(@board[index][i])
+          x_in_a_row(array)
+        end
+
+      # If we need to check the bottom half of the board
+      else
+        start = @last_row - @last_col
+        (start .. @board[0].length-1).each_with_index do |i, index|
+          array.push(@board[i][index])
+          x_in_a_row(array)
+        end
+      end
+
+      # Checks the backward diagonal of the last move (/)
+      array.clear
+      start = (@last_row) - ((@board[0].length-1) - @last_col)
+
+      # we know we need to check the bottom or half of the board
+      if start >= 0
+        (start .. @board[0].length-1).each_with_index do |i,index|
+          array.push(@board[(@board.length-1)-index][i])
+          x_in_a_row(array)
+        end
+
+      # we know we need to check the upper half of the board
+      else
+        start = @last_col - @last_row
+        (0 .. start).each_with_index do |i,index|
+          array.push(@board[index][i])
+          x_in_a_row(array)
+        end
+      end
+
+      nil
+    end
+
+  end
+
+  # Check if there is a winner
+  def x_in_a_row(row)
+    row.each_index do |i|
+
+      # Already know there is not a match
+      unless i + @win_length > row.length
+
+        # Check the part of the array by getting the unique elements
+        check = row[i,@win_length].uniq
+        unless check.first.nil? or check.count > 1
+          puts check.first
+          throw :match, check.first
+        end
+      end
+    end
+  end
+
+  def get_board()
+    @board.freeze
+  end
+
+  # Prints the current board
+  def print_board
+    @board.each do |row|
+      row.each do |i|
+        if i.nil?
+          print "| "
+        else
+          print "|",i
+        end
+      end
+      puts "|"
+    end
+
+    # Prints the bottom column numbers if less than 10.
+    unless @board[0].length > 9
+      @board[0].each_index {|i| print " ",i,""}
+      puts ""
+    end
+  end
+
+end
+
